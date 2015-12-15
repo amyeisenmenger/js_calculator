a7bc83f322f32a898facadde90c43e7221e2157b
Amy Eisenmenger <amy.eisenmenger@gmail.com>
2015-12-14 17:15:29 -0700

    show and clear history

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var historyInputOne = [];
+var historyInputTwo = [];
+var historyOperator = [];
+var historyOutput = [];
+var calculator_history = []
+
 function lastAnswer(){
   var element = document.getElementById('output');
   if (element.innerHTML == '') {
@@ -10,11 +16,14 @@ function lastAnswer(){
 function getInputs(){
   var inputOne = parseFloat(document.getElementById('input_1').value);
   var inputTwo = parseFloat(document.getElementById('input_2').value);
+  historyInputOne.push(inputOne)
+  historyInputTwo.push(inputTwo)
   return [inputOne, inputTwo];
 }
 
 function setOutput(sum){
   var lastOutput = lastAnswer();
+  historyOutput.push(sum)
   document.getElementById('output').innerHTML = sum;
   document.getElementById('last_answer').innerHTML = lastOutput;
 }
@@ -24,7 +33,7 @@ function undo(){
   document.getElementById('output').innerHTML = targetOutput;
 }
 
-function clear_calc(){
+function clearCalc(){
   document.getElementById('input_1').value = null;
   document.getElementById('input_2').value = null;
   // document.getElementById('output').innerHTML = [];
@@ -36,26 +45,46 @@ function memoryAdd(){
   document.getElementById('input_2').value = null;
 }
 
+function calcHistory(){
+  for(i = 0; i < historyInputOne.length; i++){
+    var equation_history = historyInputOne[i]+historyOperator[i]+historyInputTwo[i]+'='+historyOutput[i];
+    calculator_history.push(equation_history);
+  }
+  document.getElementById('calc_history').innerHTML = calculator_history.join('<br />')
+}
+
+function clearHistory(){
+  calculator_history = [];
+  historyInputOne = [];
+  historyInputTwo = [];
+  historyOperator = [];
+  historyOutput = [];
+}
+
 function add(){
   var inputs = getInputs();
   var sum = inputs[0] + inputs[1];
+  historyOperator.push('+')
   setOutput(sum);
 }
 
 function subtract(){
   var inputs = getInputs();
   var sum = inputs[0] - inputs[1];
+  historyOperator.push('-')
   setOutput(sum);
 }
 
 function multiply(){
   var inputs = getInputs();
   var sum = inputs[0] * inputs[1];
+  historyOperator.push('*')
   setOutput(sum);
 }
 
 function divide(){
   var inputs = getInputs();
   var sum = inputs[0] / inputs[1];
+  historyOperator.push('/')
   setOutput(sum);
 }
